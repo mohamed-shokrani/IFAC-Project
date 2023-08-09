aebcdb823e28def0928b3bc76b450b787c2c7520
mohamed-shokrani <mohamedshokrani55@gmail.com>
2023-08-09 23:20:32 +0300

    add responsive card for mobile

--- src/app/main-layout/main-layout.component.ts
+++ src/app/main-layout/main-layout.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-main-layout',
   templateUrl: './main-layout.component.html',
-  styleUrls: ['./main-layout.component.scss']
+  styleUrls: ['./main-layout.component.scss'],
 })
-export class MainLayoutComponent {
+export class MainLayoutComponent implements OnInit {
+  flag: boolean = false;
+  constructor(private breakpointObserver: BreakpointObserver) {}
+  ngOnInit(): void {
+    this.isScreen720OrMore();
+  }
 
+  isScreen720OrMore() {
+    if (this.breakpointObserver.isMatched('(width: 720px)')) {
+      this.flag = true;
+    } else this.flag = false;
+  }
 }
